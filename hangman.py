0cb247c6073fbb23176e75691a4f25764dcacd24
Eduard Khiaev <eddietheone1984@gmail.com>
2021-01-18 18:17:15 +0200

    תרגיל 4.3.1

--- hangman.py
+++ hangman.py
@@ -1,4 +1,5 @@
 import random
+import string
 
 
 LOGO = """
@@ -16,3 +17,20 @@ print(LOGO)
 print(random.randint(5, 10))
 guess = input("Guess a letter:")
 print(("_ "*len(guess)).strip())  # Order of Operations / strips the last space
+
+
+def punctuationInString(guess):
+    for char in guess:
+        if char in string.punctuation:
+            return True
+    return False
+
+
+if punctuationInString(guess) and len(guess) > 1:
+    print("E3")
+elif len(guess) > 1:
+    print("E1")
+elif punctuationInString(guess):
+    print("E2")
+else:
+    print(guess.lower())
